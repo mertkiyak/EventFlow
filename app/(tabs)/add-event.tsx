330ccef176de5d03eb3cec8e284f3b39168aa911
mertkiyak <mkiyak46@gmail.com>
2025-08-05 00:43:19 +0300

    v0.03

--- app/(tabs)/add-event.tsx
+++ app/(tabs)/add-event.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { SegmentedButtons, TextInput } from "react-native-paper";
+
+
 
 
 export default function AddEventScreen() {
     return (
         <View>
-            <Text>Login</Text>
+            <TextInput label="Event Name"  mode="outlined" />
+            <TextInput label="Description"  mode="outlined" />
+            <View>
+                <SegmentedButtons  buttons={[
+                    {value:"private", label:"Private"},
+                    {value:"public", label:"Public"}
+                    ]} />
+            </View>  
         </View>
     )
 }
